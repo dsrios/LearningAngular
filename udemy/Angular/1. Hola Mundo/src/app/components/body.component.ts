a083b49123079c4e976cfec6ea319c80ac48ab29
Diego S. Rios <diego.rios@yuxiglobal.com>
2018-01-16 14:24:24 -0500

    Clase 37 directivas *ngIf and *ngFor

--- udemy/Angular/1. Hola Mundo/src/app/components/body.component.ts
+++ udemy/Angular/1. Hola Mundo/src/app/components/body.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
 
+    // tslint:disable-next-line:no-inferrable-types
+    mostrar: boolean= false;
+
+    frase: any= {
+    mensaje: 'the true sign of intelligence is not knowledge but imagination',
+    autor: 'Albert E.'
+    };
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octopus'];
+
+
 }
