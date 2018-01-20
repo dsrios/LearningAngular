8d7f42650568007611d9e1303551fb50916b2d6c
Diego S. Rios <diego.rios@yuxiglobal.com>
2018-01-19 19:11:26 -0500

    Clase 48

--- udemy/Angular/2. SPA/src/app/app.routes.ts
+++ udemy/Angular/2. SPA/src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }// Ruta general si no hace match
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+// Usa la forma nueva de redirecionar http://localhost:4200/home
+
+
+// export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash: true});
+// Usa la forma antigua de redireccionar http://localhost:4200/#/home
+// Es una mejor forma de enviar parametros y asi hay mas navegadores que lo soportan
 
