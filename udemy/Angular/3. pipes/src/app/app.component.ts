03ec781f7594868ff4f87f77b797020e4957e029
Diego S. Rios <diego.rios@yuxiglobal.com>
2018-01-30 18:11:20 -0500

    Uso de pipes

---
+++ udemy/Angular/3. pipes/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre = 'Diego';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  number = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      carrera: '48',
+      calle: '85',
+      numero: '30',
+      casa: 'Primer Piso'
+    }
+  };
+
+}
