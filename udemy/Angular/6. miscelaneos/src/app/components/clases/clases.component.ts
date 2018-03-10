d27d7f5e4b82c03afd0d2dcdfa0269ccd9b9f6ad
Diego S. Rios <diego.rios@yuxiglobal.com>
2018-03-09 20:02:19 -0500

    Uso del ngClass para una peticion asincrona

--- udemy/Angular/6. miscelaneos/src/app/components/clases/clases.component.ts
+++ udemy/Angular/6. miscelaneos/src/app/components/clases/clases.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { fontawesome, FontAwesomeConfig } from '@fortawesome/fontawesome';
 
 @Component({
   selector: 'app-clases',
@@ -8,16 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class ClasesComponent implements OnInit {
 
   alerta: String = 'alert-danger';
+  cargar: Boolean = false;
 
   propiedades: Object = {
     danger: false
   };
 
 
-
   constructor() { }
 
   ngOnInit() {
+    // window.FontAwesomeConfig = { autoReplaceSvg: 'nest'}
+  }
+
+  ejecutar() {
+    console.log('INTRO');
+    // debugger;
+    this.cargar = true;
+    setTimeout( () => this.cargar = false, 3000 );
   }
 
 
