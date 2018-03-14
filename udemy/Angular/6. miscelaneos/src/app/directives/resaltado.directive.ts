bc98416a06ea710f8790121f64e1363a10070ad5
Diego S. Rios <diego.rios@yuxiglobal.com>
2018-03-14 10:33:01 -0500

    Directivas peronalizadas

--- udemy/Angular/6. miscelaneos/src/app/directives/resaltado.directive.ts
+++ udemy/Angular/6. miscelaneos/src/app/directives/resaltado.directive.ts
@@ -1,10 +1,33 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+
+    console.log('Directiva llamada');
+     // el.nativeElement.style.backgroundColor = 'yellow';
+    }
+
+    // tslint:disable-next-line:no-input-rename
+    @Input('appResaltado') nuevoColor: string;
+
+    @HostListener('mouseenter') mouseEntro() {
+      // console.log(this.nuevoColor);
+      // this.el.nativeElement.style.backgroundColor = 'yellow';
+
+      this.resaltar(this.nuevoColor || 'yellow');
+    }
+
+    @HostListener('mouseleave') mouseSalio() {
+      // this.el.nativeElement.style.backgroundColor = null;
+      this.resaltar( null );
+    }
+
+    private resaltar ( color: string) {
+      this.el.nativeElement.style.backgroundColor = color;
+    }
 
 }
