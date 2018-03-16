358806db9400b444401edb1bcf480352fdbcff7a
Diego S. Rios <diego.rios@yuxiglobal.com>
2018-03-16 17:00:41 -0500

    Creacion rutas hijas

--- udemy/Angular/6. miscelaneos/src/app/app.routes.ts
+++ udemy/Angular/6. miscelaneos/src/app/app.routes.ts
@@ -5,13 +5,29 @@ import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
 
+// Rutas hijas
+import { NuevoUsuarioComponent } from './components/usuario/nuevo-usuario.component';
+import { EditarUsuarioComponent } from './components/usuario/editar-usuario.component';
+import { DetalleUsuarioComponent } from './components/usuario/detalle-usuario.component';
 
 
+
+// usuario/10/nuevo
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'user', component: UsuarioComponent }
-    // { path: 'path', component: FeatureComponent },
-    // { path: '**', component: HomeComponent }
+    { // Ruta principal
+        path: 'user/:id',
+        component: UsuarioComponent,
+        // Ruta hija
+        children: [
+                    {path: 'nuevo', component: NuevoUsuarioComponent},
+                    {path: 'editar', component: EditarUsuarioComponent},
+                    {path: 'detalle', component: DetalleUsuarioComponent},
+                    { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+
+                  ]
+    },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 @NgModule({
