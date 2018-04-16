5302d9db6fd1309a57a97addee925eeee6fa27ed
Diego S. Rios <diego.rios@yuxiglobal.com>
2018-04-16 17:39:34 -0500

    formularios con from data

--- udemy/Angular/formularios/src/app/components/data/data.component.ts
+++ udemy/Angular/formularios/src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'; // Necesario para la aproximacion por data
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'; // Necesario para la aproximacion por data
 
 @Component({
   selector: 'app-data',
@@ -18,37 +18,77 @@ export class DataComponent implements OnInit {
       nombre: 'Diego',
       apellido: 'Perez'
     },
-    correo: 'diego@yopmail.com'
-  };
+    correo: 'diego@yopmail.com',
+    pasatiempos: ['Correr', 'Jugar Futbol', 'Dormir', 'Ir a cine']
+    };
 
   constructor() {
 
     console.log(this.usuario);
 
+
+    /*
+      Ejemplo y uso del FormGroup
+      nameInput: new FormControl (valueDefautl, [validationRules], validationRulesAsync)
+      Se incluyeron en el nombrecompleto dentro del FormGroup
+      'nombre': new FormControl( '', [Validators.required, Validators.minLength(3)] ),
+      'apellido': new FormControl( '', Validators.required ),
+    */
+
     this.forma = new FormGroup({
 
       'nombrecompleto': new FormGroup({
           'nombre': new FormControl( '', [Validators.required, Validators.minLength(3)] ),
-          'apellido': new FormControl( '', Validators.required )
+          'apellido': new FormControl( '', [Validators.required, this.noDiego] )
         }),
-
-      // nameInput: new FormControl (valueDefautl, [validationRules], validationRulesAsync)
-      // Se incluyeron en el nombrecompleto dentro del FormGroup
-      // 'nombre': new FormControl( '', [Validators.required, Validators.minLength(3)] ),
-      // 'apellido': new FormControl( '', Validators.required ),
-      'correo': new FormControl( '', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9._]+\.[a-z]{2,3}$')] )
+      'correo': new FormControl( '', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9._]+\.[a-z]{2,3}$')] ),
+      'pasatiempos': new FormArray([
+        new FormControl('', Validators.required)
+      ])
     });
 
     // Agregar los valores a los campos. (la forma debe tener la misma forma que el objeto)
-    this.forma.setValue ( this.usuario );
+    // this.forma.setValue ( this.usuario );
 
   }
 
   ngOnInit() {
   }
 
+
+  agregarPasatiempo() {
+    /*
+    <FormArray> : Esta sintaxis sirve para que TS sepa que el elemento que
+    estamos seleccionando es un arreglo
+    */
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl( '', Validators.required )
+    );
+  }
+
+
+  // Crear validaciones personalizadas (Se pueden crear en un documento aparte)
+  noDiego( control: FormControl ): {[s: string]: boolean} {
+    if ( control.value === 'diego' ) {
+      return {
+        noDiego: true
+      };
+    }
+
+    return null;
+  }
+
   GuardarCambios() {
     console.log( this.forma.value );
     console.log( this.forma );
+
+    // Resetear el formulario a su estado pristing (estado inicial)
+    this.forma.reset ( {
+      nombrecompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+    } );
   }
 }
